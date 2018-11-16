f42d08f0f96a95e6707d619a483b9913fb71173e
fer101 <33917211+fer101@users.noreply.github.com>
2018-11-15 20:36:59 -0600

    Update global.php
    updated

--- resources/lang/en/global.php
+++ resources/lang/en/global.php
@@ -83,8 +83,8 @@ return [
     'treatment_plan' => 'Treatment Plan',
     'interconsultation' => 'Interconsultation',
     'treatment' => 'Treatment',
-    
-
+    'exit' => 'Exit',
+    'save_patient' => 'Save Patient'
 
     
 ];
