01d18f4f311949b30b43c5743104941ceb16e68b
Fernando Santana <fernando_19_1996@hotmail.com>
2018-11-21 19:45:45 -0600

    prescripciones

---
+++ app/Http/Controllers/PrescriptionController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrescriptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("doctor.prescriptions_new");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'patient.name' => 'required',
+            'patient.lastname' => 'required',
+            'patient.sex' => 'required',
+            'patient.email' => 'email'
+        ]);
+    }
+}
